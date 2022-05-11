ce283475180ff6d8398c6bf105c767808a682206
mauricioLeite <mauricioleite.fe@gmail.com>
2022-05-11 09:45:15 -0300

    feat: create registry routes

--- blockchain/urls.py
+++ blockchain/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path 
 
+from .views.registry import RegistryView, PendingView, MineView
+
 urlpatterns = [
-    # path("registry/", ClassView.as_view()),
+    path("registry/", RegistryView.as_view()),
+    path("registry/<int:id_>", RegistryView.as_view()),
+
+    path("pending/", PendingView.as_view()),
+    path("mine/", MineView.as_view()),
 ]
