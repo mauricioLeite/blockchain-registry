e058f422c0c98f046110d82314cc9262954b5ae2
mauricioLeite <mauricioleite.fe@gmail.com>
2022-05-15 20:45:50 -0300

    feat: add peers injection and add mine response handler

--- blockchain/services/registry_service.py
+++ blockchain/services/registry_service.py
@@ -1,4 +1,3 @@
-import logging, json
 from typing import Union
 
 from rest_framework.response import Response
@@ -7,14 +6,16 @@ from rest_framework import status
 from .registry_operations import RegistryOperations
 class RegistryService:
 
-    def __init__(self, operations: RegistryOperations) -> None:
+    def __init__(self, operations: RegistryOperations, peers: list = None) -> None:
         self.__operations = operations
+        self.__peers = peers
         
     def list(self, id_: Union[str,int] = None):
         chain = self.__operations.list(id_)
-        return Response({"chain": chain, "length": len(chain)}, status.HTTP_200_OK)
+        return Response({"chain": chain, "peers": self.__peers, "length": len(chain)}, status.HTTP_200_OK)
 
     def insert_registry(self, payload: dict) -> Response:
+        # TODO: Need validation on payload strucuture
         confirm = self.__operations.insert_new_block(payload)
         if confirm:
             return Response({"message":"Transaction requested!"}, status.HTTP_201_CREATED)
@@ -25,5 +26,8 @@ class RegistryService:
         return Response({"unconfirmed_transactions": pending_tx, "length": len(pending_tx)}, status.HTTP_200_OK)
 
     def mine(self):
-        chain = self.__operations.mine()
-        return Response({"chain": chain, "length": len(chain)}, status.HTTP_200_OK)
+        # TODO: verify existence of pending transactions
+        block = self.__operations.mine()
+        if not block: return Response({"message": "No transaction to mine."}, status.HTTP_200_OK)
+        
+        return Response({"block": block}, status.HTTP_200_OK)
