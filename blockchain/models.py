c1a8593cbc94e558b474675fb04b520ead826ae4
mauricioLeite <mauricioleite.fe@gmail.com>
2022-06-07 07:21:00 -0300

    feat: create models on django structure

--- blockchain/models.py
+++ blockchain/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Blocks(models.Model):
+    block = models.JSONField(blank=False, null=False)
+
+class PendingTransactions(models.Model):
+    transcation = models.JSONField(blank=False, null=False)
