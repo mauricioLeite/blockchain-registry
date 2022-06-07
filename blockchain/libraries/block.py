7f54a73a169f6443ad75aec2ef748c7db1fd57ee
mauricioLeite <mauricioleite.fe@gmail.com>
2022-06-07 07:25:16 -0300

    feat: add hash on block properties

--- blockchain/libraries/block.py
+++ blockchain/libraries/block.py
@@ -2,12 +2,13 @@ import json
 from hashlib import sha256
 
 class Block:
-    def __init__(self, index, transaction, timestamp, previous_hash, nonce=0):
+    def __init__(self, index, transaction, timestamp, previous_hash, nonce=0, hash=None):
         self.index = index
         self.transaction = transaction
         self.timestamp = timestamp
         self.previous_hash = previous_hash
         self.nonce = nonce
+        self.hash = hash
 
     def compute_hash(self):
         block_string = json.dumps(self.__dict__, sort_keys=True)
