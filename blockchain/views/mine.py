f8a40a24c672c185b577e3d76779a0c8fb0c3990
mauricioLeite <mauricioleite.fe@gmail.com>
2022-07-08 18:49:38 -0300

    feat: create MineView class

---
+++ blockchain/views/mine.py
@@ -0,0 +1,19 @@
+import json, logging
+
+from rest_framework.views import APIView
+
+
+from ..services.registry_service import RegistryService
+
+from blockchain.libraries.peers_manager import PeersManager
+from ..libraries.blockchain import Blockchain
+from adapters.factory import DjangoStorageFactory
+
+factory = DjangoStorageFactory()
+blockchain = Blockchain(factory)
+peers = PeersManager(factory)
+
+class MineView(APIView):
+
+    def get(self, _):
+        return RegistryService().mine()
