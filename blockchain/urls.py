4e24ed68fb595ebfbcd8fb1b1752350e82143cfd
mauricioLeite <mauricioleite.fe@gmail.com>
2022-07-08 18:50:29 -0300

    feat: update views classes on routes

--- blockchain/urls.py
+++ blockchain/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path 
 
 from blockchain.views.registry import RegistryView
-from blockchain.views.transactions import  PendingView, MineView
+from blockchain.views.transactions import  TransactionsView
+from blockchain.views.mine import MineView
 from blockchain.views.nodes import NewNodeView, RegisterNodeView, BlockSyncView
 
 urlpatterns = [
     path("registry/", RegistryView.as_view()),
     path("registry/<int:id_>", RegistryView.as_view()),
 
-    path("pending/", PendingView.as_view()),
+    path("pending/", TransactionsView.as_view()),
+    
     path("mine/", MineView.as_view()),
 
     path("register_node/", NewNodeView.as_view()),
