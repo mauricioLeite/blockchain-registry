0466f863307dfd55d120a2ba94b9bc7c640f2a31
mauricioLeite <mauricioleite.fe@gmail.com>
2022-07-08 18:25:34 -0300

    feat: update registryOperations to support database

--- blockchain/services/registry_operations.py
+++ blockchain/services/registry_operations.py
@@ -1,34 +1,42 @@
 from typing import Union
 import requests, json
 
+from adapters.factory import DjangoStorageFactory
+
 from ..libraries.blockchain import Blockchain
+from ..libraries.peers_manager import PeersManager
 
 class RegistryOperations:
 
-    def __init__(self, blockchain: Blockchain, peers: list = None) -> None:
+    def __init__(self, blockchain: Blockchain, storage: DjangoStorageFactory = None, peers: PeersManager = None) -> None:
         self.__blockchain = blockchain
-        self.__peers = peers
+        self.storage = storage
+        self.peers = peers
 
     def list(self, id_: Union[str, int] = None):
+        print(self.__blockchain.chain)
+        peers = self.peers.list()
         if id_:
-            return self.__blockchain.get_block(id_)
-
-        return self.__blockchain.chain
+            block = self.__blockchain.get_block(id_)
+            return block, peers
+        return self.__blockchain.chain, peers
 
     def insert_new_block(self, data: dict):
         self.__blockchain.add_new_transaction(data)
         return True
         
     def mine(self):
-        mined_block_id = self.__blockchain.mine()
+        unconfirmed_transaction = self.storage.createPendingTransactionsModel().first()
+        mined_block_id = self.__blockchain.mine(unconfirmed_transaction)
         if not mined_block_id: return None
 
-        # chain_length = len(self.__blockchain.chain)
+        chain_length = len(self.__blockchain.chain)
         # self.consensus()
-        # if chain_length == len(self.__blockchain.chain):
-        #     self.announce_new_block(self.__blockchain.last_block)
+        if chain_length == len(self.__blockchain.chain):
+            self.announce_new_block(self.__blockchain.last_block)
+            self.storage.createPendingTransactionsModel().delete({ "id": unconfirmed_transaction["id"] })
         
-        return self.__blockchain.last_block.__dict__
+        return self.__blockchain.last_block
 
     
     def consensus(self):
@@ -38,7 +46,8 @@ class RegistryOperations:
         """
         longest_chain = None
         current_len = len(self.__blockchain.chain)
-        for node in self.__peers:
+        peers = self.storage.createPeersModel().get_all()
+        for _, node in peers.items():
             response = requests.get(f"http://{node}/registry")
             length = response.json()['length']
             chain = response.json()['chain']
@@ -58,10 +67,9 @@ class RegistryOperations:
         Other blocks can simply verify the proof of work and add it to their
         respective chains.
         """
-        for peer in self.__peers:
-            requests.post(f"http://{peer}/sync_block/", json=block.__dict__)
-
-
+        peers = self.storage.createPeersModel().get_all()
+        for _, node in peers.items():
+            requests.post(f"http://{node}/sync_block/", json=block)
 
     def pending_transactions(self):
-        return self.__blockchain.unconfirmed_transactions
+        return self.storage.createPendingTransactionsModel().get_all()
