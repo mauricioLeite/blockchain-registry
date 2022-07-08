5fbc77d90ba25b9d91dd338897d196fa49b9b5b8
mauricioLeite <mauricioleite.fe@gmail.com>
2022-07-08 18:25:10 -0300

    feat: update blockchain class to use database

--- blockchain/libraries/blockchain.py
+++ blockchain/libraries/blockchain.py
@@ -8,31 +8,51 @@ class Blockchain:
 
     def __init__(self, factory: DjangoStorageFactory):
         self.storage = factory
-        self.unconfirmed_transactions = []
-        # self.chain = []
         self.__create_genesis_block()
  
-    def __create_genesis_block(self):
+    def __create_genesis_block(self, reference: Block = None):
         blocks_model = self.storage.createBlockModels()
         if blocks_model.count_rows() == 0:
-            genesis_block = Block(0, [], time.time(), "0")
-            genesis_block.hash = genesis_block.compute_hash()
-            self.storage.createBlockModels().insert(genesis_block)
+            if not reference:
+                genesis_block = Block(0, [], time.time(), "0")
+                genesis_block.hash = genesis_block.compute_hash()
+            else:
+                del reference["id"]
+                genesis_block = Block(**reference)
+            self.storage.createBlockModels().insert(genesis_block.__dict__)
+            
 
     @property
     def chain(self):
         return self.storage.createBlockModels().get_all()
     
     @property
-    def get_block(self, id_):
-        return self.storage.createBlockModels().get({"id": id_})
-
-    @property
     def last_block(self):
-        return Block(**self.storage.createBlockModels().last())
+        return self.storage.createBlockModels().last()
+        
+    def get_block(self, id_: int):
+        block = self.storage.createBlockModels().get({"id": id_})
+        return block if block else []
+
+    # New transaction logic
+    def add_new_transaction(self, transaction: dict):
+        self.storage.createPendingTransactionsModel().insert({ "transaction": transaction })
 
     #   NEW BLOCKS LOGIC
-    def proof_of_work(self, block):
+    def mine(self, unconfirmed_transaction: dict):
+        del unconfirmed_transaction["created_at"]
+        last_block = self.last_block
+        
+        new_block = Block(index=last_block["index"] + 1,
+                          transaction=unconfirmed_transaction,
+                          previous_hash=last_block["hash"])
+
+        proof = self.proof_of_work(new_block)
+        id_ = self.add_block(new_block, proof)
+        return id_, unconfirmed_transaction["id"]
+
+
+    def proof_of_work(self, block: Block):
         # TODO: internalize nonce compute
         block.nonce = 0
         computed_hash = block.compute_hash()
@@ -42,35 +62,20 @@ class Blockchain:
         return computed_hash
 
     def add_block(self, block, proof):
-        previous_hash = self.last_block.hash
+        previous_hash = self.last_block["hash"]
         if previous_hash != block.previous_hash:
             return False
+
         if not self.is_valid_proof(block, proof):
             return False
         block.hash = proof
-        return self.storage.createBlockModels().insert(block)
+        return self.storage.createBlockModels().insert(block.__dict__)
  
     def is_valid_proof(self, block, block_hash):
         return (block_hash.startswith("0" * Blockchain.difficulty) and
             block_hash == block.compute_hash())
 
-    def add_new_transaction(self, transaction):
-        self.unconfirmed_transactions.append(transaction)
- 
-    def mine(self):
-        if not self.unconfirmed_transactions:
-            return False
- 
-        last_block = self.last_block
-        new_block = Block(index=last_block.index + 1,
-                          transaction=self.unconfirmed_transactions[0],
-                          previous_hash=last_block.hash)
- 
-        proof = self.proof_of_work(new_block)
-        id_ = self.add_block(new_block, proof)
-        self.unconfirmed_transactions = self.unconfirmed_transactions[1:]
-        return id_
-        
+    
     # CONSENSUS LOGIC
     '''
         Check if entire Blockchain is valid
@@ -89,3 +94,26 @@ class Blockchain:
             block.hash, previous_hash = block_hash, block_hash
 
         return result
+
+
+    # Sync new node
+    def create_chain_from_dump(self, chain_dump):
+        #TODO: implement chain roolback
+        before = self.storage.createBlockModels().get_all()
+        self.storage.createBlockModels().delete()
+        after = self.storage.createBlockModels().get_all()
+        print(f"before: {len(before)} --- after: {len(after)}")
+        
+        for block_data in chain_dump:
+            if block_data["index"] == 0:
+                self.__create_genesis_block(block_data)
+            else:
+                del block_data["id"]
+                proof = block_data['hash']
+                del block_data['hash']
+                block = Block(**block_data)
+                added = self.add_block(block, proof)
+                if not added:
+                    raise Exception("The chain dump is tampered!!")
+            
+        return self.chain
