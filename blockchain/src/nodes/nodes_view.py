761edd256581833f0cc989b858ec0c6f227c6bdc
mauricioLeite <mauricioleite.fe@gmail.com>
2022-07-11 20:39:12 -0300

    feat: finish BlockSync service

--- blockchain/src/nodes/nodes_view.py
+++ blockchain/src/nodes/nodes_view.py
@@ -18,14 +18,14 @@ class NewNodeView(APIView):
 
     def post(self, request):
         payload = request.data
-        logging.info(json.dumps({ "payload": payload }, ensure_ascii=False))
+        logging.info(json.dumps({ "class": "NewNodeView" , "payload": payload }, ensure_ascii=False))
         return NodeService(storage, library).new_node(payload)
 
 #TODO: remove class after tests
 class ClearLocalPeers(APIView):
 
     def get(self, request):
-        logging.info(json.dumps({ "message": "Clear request received!" }, ensure_ascii=False))
+        logging.info(json.dumps({ "class": "ClearLocalPeers" , "message": "Clear request received!" }, ensure_ascii=False))
         return NodeService(storage, library).clear_local()
 
 
@@ -36,7 +36,7 @@ class JoinView(APIView):
     def post(self, request):
         payload = request.data
         host = request.get_host()
-        logging.info(json.dumps({ "payload": payload, "host": host }, ensure_ascii=False))
+        logging.info(json.dumps({ "class": "JoinView", "payload": payload, "host": host }, ensure_ascii=False))
         return NodeService(storage, library).join_network(payload, host)
     
 """
@@ -45,15 +45,8 @@ class JoinView(APIView):
 class BlockSyncView(APIView):
 
     def post(self, request):
-        payload = request.data
-        logging.info(json.dumps({ "payload": payload, "class": "BlockSync"}, ensure_ascii=False))
+        payload = json.loads(request.data)
+        logging.info(json.dumps({ "class": "BlockSync", "payload": payload}, ensure_ascii=False))
+        return NodeService(storage, library).sync_block(payload)
         
-        block = Block(payload.get("index"), payload.get("transaction"), payload.get("created_at"), payload.get("previous_hash"), payload.get("nonce"))
-
-        proof = payload.get("hash")
-        added = blockchain.add_block(block, proof)
-
-        if not added: return Response({"message":"The block is discarded by the node."}, status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-        return Response({"message": "Block added to the chain"}, status.HTTP_201_CREATED)
     
