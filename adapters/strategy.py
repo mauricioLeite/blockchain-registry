6fdafaca0605126cef2b2e7b83ab8421ea84ac7b
mauricioLeite <mauricioleite.fe@gmail.com>
2022-10-24 19:48:24 -0300

    feat: create StrategyModel mock to implements

---
+++ adapters/strategy.py
@@ -0,0 +1,26 @@
+"""
+Implents adapter method based on Django ORM
+"""
+
+class StrategyModel():
+
+    def get(self):
+        pass
+    
+    def get_all(self):
+        pass
+    
+    def first(self):
+        pass
+    
+    def last(self):
+        pass
+    
+    def count_rows(self):
+        pass
+    
+    def insert(self):
+        pass
+
+    def delete(self):
+        pass
